5fdbc7e7019cb89101af7c5b0fdcbad0b7202946
lishuo0710 <ltby610@163.com>
2017-10-12 21:35:42 +0800

    alter a parameter

--- src/main/java/org/seckill/dao/SuccessKilledDao.java
+++ src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -1,5 +1,6 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.SuccessKilled;
 
 public interface SuccessKilledDao {
@@ -10,12 +11,12 @@ public interface SuccessKilledDao {
      * @param userPhone
      * @return 如果影响行数>1
      */
-    int insertSuccessKilled(long seckillId,long userPhone);
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
 
     /**
      * 根据Id查询秒杀明细并携带产品对象
      * @param seckillId
      * @return 秒杀明细
      */
-    SuccessKilled queryByIdWithSeckill(long seckillId);
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId")long seckillId);
 }
